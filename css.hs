e9cb2011a22e0fbdf4b6ca077789a909e064d081
Daniel Barter <danielbarter@gmail.com>
2016-01-28 16:02:17 -0500

    i suck.

--- css.hs
+++ css.hs
@@ -1,19 +1,3 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Clay
-
-import Clay
-
-main = putCss theStylesheet
-
-
-
-
-theStylesheet = 
-
-  do
-    body ?
-      do background  black
-         color       green
-         border      dashed (px 2) yellow
 
