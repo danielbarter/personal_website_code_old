fe0c7f071e4bb31b7f1ca158fd71b37aa60fe4b1
Daniel Barter <danielbarter@gmail.com>
2018-04-08 13:22:53 +1000

    stashing

--- CALGO/max_binary_heap/max_binary_heap.c
+++ CALGO/max_binary_heap/max_binary_heap.c
@@ -7,9 +7,10 @@ typedef struct
   int length;
 
   /*
-    heap size represents how many elements in the heap are stored in array.
-    The heap is array[1..heap_size] where 0 <= heap_size <= length.
-    The array is array[1..length]
+    length of heap. Not all elements in the array are part of the heap.
+    The array is array[1..length].
+    The heap is array[1..heap_size].
+    When heap_size < length, then array[heap_size + 1..length] is extra space.
    */
   int heap_size;
 
@@ -21,12 +22,23 @@ typedef struct
 
 } heap;
 
+void *get(heap *h, int i);
+void *set(heap *h, int i, void *ptr);
+int parent(int i);
+int left(int i);
+int right(int i);
+void max_heapify(heap *h, int i);
+void build_max_heap(heap *h);
+void heapsort(heap *h);
+
+
+
 /*
   in CLRS, arrays begin their indexing at 1. For Binary heaps, this makes the algorithms look a bit nicer, so we shall use their indexing, with getters and setters to update the array.
 */
 
-inline void *get(heap *h, int i){ return h->array[i+1];}
-inline void *set(heap *h, int i, void *ptr){ return (h->array[i+1]) = ptr;}
+inline void *get(heap *h, int i){ return h->array[i-1];}
+inline void *set(heap *h, int i, void *ptr){ return (h->array[i-1]) = ptr;}
 
 
 /* the data in a binary heap represents a complete binary tree. */
@@ -35,4 +47,80 @@ inline int parent(int i){ return i/2;}
 inline int left(int i){ return 2*i;}
 inline int right(int i){ return 2*i + 1;}
 
-/* In a max binary heap, the max property is array[parent(i)] >= array[i] */
+/*
+  In a max binary heap, the max property is array[parent(i)] >= array[i]
+
+  when called, max_heapify assumes that the binary trees rooted at left(i)
+  and right(i) are max heaps, but array[i] might be smaller than its children.
+
+  max_heapify lets the value at array[i] float down, so that the subtree rooted
+  at index i obeys the max heap property.
+
+*/
+
+void max_heapify(heap *h, int i)
+{
+  int l = left(i);
+  int r = right(i);
+
+  int largest;
+  void *temp;
+
+  /* determine which is larger out of A[i], A[l], A[r] */
+  if ((l <= h->heap_size) && ( (*h->compare)( get(h,i), get(h,l) ) ) )
+    largest = l;
+  else largest = i;
+
+  if ((r <= h->heap_size)  && ( (*h->compare)( get(h,largest), get(h,r) ) ) )
+    largest = r;
+
+
+
+  if (largest != i)
+    {
+      temp = get(h,largest);
+      set(h, largest, get(h,i));
+      set(h, i, temp);
+      max_heapify(h,largest);
+    }
+}
+
+/*
+  the build max heap procedure should be passed a heap data structure with:
+    the data contained in h.array.
+    the array length h.length.
+    h.heap_size = 0
+    h.compare the comparison procedure pointer.
+ */
+
+void build_max_heap(heap *h)
+{
+  int i;
+
+  h->heap_size = h->length;
+
+  for (i = h->length/2; i >= 1;i--)
+    max_heapify(h,i);
+}
+
+/*
+  the build max heap procedure should be passed a heap data structure with:
+  the data contained in h.array.
+  the array length h.length.
+  h.heap_size = 0
+  h.compare the comparison procedure pointer.
+*/
+void heapsort(heap *h)
+{
+  int i;
+  void *temp;
+  build_max_heap(h);
+  for (i = h->length; i >= 2; i--)
+    {
+      temp = get(h,1);
+      set(h,1,get(h,i));
+      set(h,i,temp);
+      h->heap_size--;
+      max_heapify(h,1);
+    }
+}
--- CALGO/max_binary_heap/test.c
+++ CALGO/max_binary_heap/test.c
@@ -1,5 +1,107 @@
+/* system includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "max_binary_heap.c"
 
-void main(void)
+void print_heap(heap *h);
+int int_compare_32bit(void *ptr1, void *ptr2);
+void test_build_max_heap(int array_length, int max_value);
+void test_mergesort(int array_length, int max_value);
+
+
+/* for all the testing, we will work with boxed 32 bit integers */
+
+int main(void)
 {
+  srand(time(NULL));  /*   initialize the seed for the pseudorandom generator   */
+
+
+  test_build_max_heap(0,100);
+  test_build_max_heap(1,100);
+  test_build_max_heap(10,100);
+  test_build_max_heap(50,10);
+
+  test_mergesort(0,100);
+  test_mergesort(1,100);
+  test_mergesort(10,100);
+  test_mergesort(50,10);
+  return 0;
+}
+
+
+void print_heap(heap *h)
+{
+  int i;
+
+  printf("array length: %d\n",h->length);
+  printf("heap size: %d\n",h->heap_size);
+
+  printf("array values: ");
+  for(i = 0; i < h->length;i++)
+    printf("%d ",* (int *) h->array[i]);
+
+}
+
+void test_build_max_heap(int array_length, int max_value)
+{
+  int values[array_length];
+  void *pointers[array_length];
+  int i;
+
+  for (i=0; i<array_length;i++)
+    values[i] = rand() % max_value;
+
+
+  for(i=0; i<array_length;i++)
+    pointers[i] = values + i;
+
+  heap h = {.array = pointers,
+            .length = array_length,
+            .heap_size = 0,
+            .compare = &int_compare_32bit};
+
+  printf("\n\n");
+  printf("heap init..............\n");
+  print_heap(&h);
+  printf("\n\napplying build max heap procedure............\n\n");
+  build_max_heap(&h);
+  printf("resulting heap:\n");
+  print_heap(&h);
+  printf("\n\n");
+}
+
+void test_mergesort(int array_length, int max_value)
+{
+  int values[array_length];
+  void *pointers[array_length];
+  int i;
+
+  for (i=0; i<array_length;i++)
+    values[i] = rand() % max_value;
+
+
+  for(i=0; i<array_length;i++)
+    pointers[i] = values + i;
+
+  heap h = {.array = pointers,
+            .length = array_length,
+            .heap_size = 0,
+            .compare = &int_compare_32bit};
+
+  printf("\n\n");
+  printf("heap init..............\n");
+  print_heap(&h);
+  printf("\n\napplying heapsort............\n\n");
+  heapsort(&h);
+  printf("resulting heap:\n");
+  print_heap(&h);
+  printf("\n\n");
+}
+
+
+int int_compare_32bit(void *ptr1, void *ptr2)
+{
+  return *((int *) ptr1) < *((int *) ptr2);
 }
